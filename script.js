fec960ddcb39eadff2e89c00b1373dc52311480c
jopit59 <pittera.joseph@gmail.com>
2022-11-19 17:50:53 +0100

    adding scroll 100vh

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
-/* element visibility on viewport visibility */
-/* var observer = new IntersectionObserver(
-  function (entries) {
-    console.log(entries);
-    if (entries[0]["isIntersecting"] === true)
-      document
-        .querySelector("#ws-presentation")
-        .classList.add("text-appearance");
-    else
-      document
-        .querySelector("#ws-presentation")
-        .classList.remove("text-appearance");
-  },
-  { threshold: [1] }
-);
-observer.observe(document.querySelector("#ws-presentation")); */
-
 // NAVBAR
 let lastScrollTop = 0;
 let navbar = document.getElementById("navbar");
+let main = document.getElementsByName("main");
 window.addEventListener("scroll", function () {
   const scrollTop =
     window.pageTOffset || this.document.documentElement.scrollTop;
