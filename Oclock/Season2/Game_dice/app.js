6512317e1cd535d15d882551c49017ff3847dd08
jopit59 <pittera.joseph@gmail.com>
2022-11-22 09:30:46 +0100

    set up setTimeout to smooth the interactions

--- Oclock/Season2/Game_dice/app.js
+++ Oclock/Season2/Game_dice/app.js
@@ -1,3 +1,4 @@
+// Create the 2 div const that will contain the player's and dealer's dices
 const playerDiv = document.createElement("div");
 playerDiv.classList.add("board");
 playerDiv.id = "player";
@@ -7,19 +8,38 @@ dealerDiv.classList.add("board");
 dealerDiv.id = "dealer";
 dealerDiv.innerHTML = "<h2>DEALER</h2>";
 
+// initialize the const to point to HTML elements
 const app = document.querySelector("#app");
-app.appendChild(playerDiv);
-app.appendChild(dealerDiv);
 const lancer = document.querySelector("#button");
-lancer.addEventListener("click", play);
 const nbDice = document.querySelector("#nbDice");
 
+// add the player's and dealer's div to the DOM
+app.appendChild(playerDiv);
+app.appendChild(dealerDiv);
+
+// add the click event on the "Lancer" button
+lancer.addEventListener("click", play);
+
+/**
+ * take 2 numbers and return a random number between min and max
+ * @param {Number} min
+ * @param {Number} max
+ * @returns {Number}
+ */
 function randomNumber(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
 
+/**
+ * create a div with an image of Dice in the HTML element player
+ * switch position on X-axis depending on the face "n" of the dice
+ * and return the sum of the dices
+ * @param {HTMLElement} player
+ * @param {Number} n
+ * @returns {Number}
+ */
 function createDice(player, n) {
-  count = 0;
+  let count = 0;
   for (let i = 0; i < n; i++) {
     const diceDiv = document.createElement("div");
     diceDiv.classList.add("dice");
@@ -29,8 +49,15 @@ function createDice(player, n) {
       (20 * (diceNumber - 1)).toString() + "%";
     count += diceNumber;
   }
+  return count;
 }
 
+/**
+ * display the results in a new div below the div with Dice
+ * taking into account the sum of the dices
+ * @param {HTMLElement} player
+ * @param {Number} count
+ */
 function addResults(player, count) {
   const resultDiv = document.createElement("div");
   resultDiv.classList.add("results");
@@ -40,6 +67,12 @@ function addResults(player, count) {
     count.toString() +
     "</span>";
 }
+
+/**
+ * display the winner message in the right div, player or dealer
+ * @param {HTMLElement} player
+ * @param {String} msg
+ */
 function winnerIs(player, msg) {
   const winnerDiv = document.createElement("div");
   winnerDiv.classList.add("winner");
@@ -47,26 +80,43 @@ function winnerIs(player, msg) {
   player.appendChild(winnerDiv);
 }
 
-let count = 0;
-
+/**
+ * main function to play the game
+ *
+ */
 function play() {
+  // initialize the div player and dealer
   playerDiv.innerHTML = "<h2>PLAYER</h2>";
   dealerDiv.innerHTML = "<h2>DEALER</h2>";
+
+  // initialize variables
   let numberOfDice = nbDice.value;
-  createDice(playerDiv, numberOfDice);
-  addResults(playerDiv, count);
-  createDice(dealerDiv, numberOfDice);
-  addResults(dealerDiv, count);
-  const scores = document.querySelectorAll("span");
-  // debugger;
-  if (parseInt(scores[0].textContent) > parseInt(scores[1].textContent)) {
-    winnerIs(playerDiv, "Vous avez GAGNE !");
-  } else if (
-    parseInt(scores[0].textContent) < parseInt(scores[1].textContent)
-  ) {
-    winnerIs(dealerDiv, "Vous avez GAGNE !");
-  } else {
-    winnerIs(playerDiv, "EGALITE");
-    winnerIs(dealerDiv, "EGALITE");
-  }
+  let scorePlayer = 0;
+  let scoreDealer = 0;
+
+  scorePlayer = createDice(playerDiv, numberOfDice);
+  setTimeout(() => {
+    addResults(playerDiv, scorePlayer);
+  }, 2000);
+
+  setTimeout(() => {
+    scoreDealer = createDice(dealerDiv, numberOfDice);
+  }, 1000);
+  setTimeout(() => {
+    addResults(dealerDiv, scoreDealer);
+  }, 2000);
+
+  setTimeout(() => {
+    const scores = document.querySelectorAll("span");
+    if (parseInt(scores[0].textContent) > parseInt(scores[1].textContent)) {
+      winnerIs(playerDiv, "Vous avez GAGNE !");
+    } else if (
+      parseInt(scores[0].textContent) < parseInt(scores[1].textContent)
+    ) {
+      winnerIs(dealerDiv, "Vous avez GAGNE !");
+    } else {
+      winnerIs(playerDiv, "EGALITE");
+      winnerIs(dealerDiv, "EGALITE");
+    }
+  }, 2500);
 }
