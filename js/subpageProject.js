96104c93fc8d4cd9c16a486f956ce23d508e886d
jopit59 <pittera.joseph@gmail.com>
2022-12-04 15:11:46 +0100

    reorganize files and add theme on subpages

--- js/subpageProject.js
+++ js/subpageProject.js
@@ -1,13 +1,20 @@
 const theme = {
   bg: document.querySelectorAll(".bg"),
+  projectTile: document.querySelectorAll(".project"),
 
   init() {
     if (localStorage.getItem("theme") === "dark") {
       theme.bg.forEach((x) => x.classList.replace("light-bg", "dark-bg"));
+      theme.projectTile.forEach((x) =>
+        x.classList.replace("--project-light", "--project-dark")
+      );
     } else {
       theme.bg.forEach((x) => x.classList.replace("dark-bg", "light-bg"));
+      theme.projectTile.forEach((x) =>
+        x.classList.replace("--project-dark", "--project-light")
+      );
     }
   },
 };
 
-document.body.onload = theme.init;
+theme.init();
